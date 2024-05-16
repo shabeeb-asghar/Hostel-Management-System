8654e74102370982fdab0c071327ac5cb9511e00
Shabeeb-Asghar <shabeebasghar57@gmail.com>
2024-05-17 00:02:08 +0500

    Landing Page

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Landing_page.fxml"));
+
+        Scene scene = new Scene(root, 626, 425);
+        scene.getStylesheets().add(getClass().getResource("Landing_Page_Style.css").toExternalForm());
+        primaryStage.setScene(scene);
+
+        primaryStage.setTitle("HMS");
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
