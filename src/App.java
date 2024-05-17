8b5e0510fbed2029021a3476aa6e247bfe0d2bea
Shabeeb-Asghar <shabeebasghar57@gmail.com>
2024-05-17 20:40:30 +0500

    Login Page Intitial Design

--- src/App.java
+++ src/App.java
@@ -8,10 +8,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Landing_page.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login_Page.fxml"));
 
         Scene scene = new Scene(root, 626, 425);
-        scene.getStylesheets().add(getClass().getResource("Landing_Page_Style.css").toExternalForm());
         primaryStage.setScene(scene);
 
         primaryStage.setTitle("HMS");
